1bc87a53a6ea9124afacc7b4bc697c2d4666ddc6
Brandon Leon <brandon.leon@servicenow.com>
2024-11-14 09:37:41 -0800

    feat: enhance duration formatting and calculation logic
    Update the format_duration function to accept a from_now parameter,
    allowing for more flexible human-readable time formatting. Adjust
    calculate_delta_seconds to handle cases where the end timestamp
    is not provided, defaulting to the current time.
    
    Improve output clarity by ensuring the elapsed time message reflects
    the correct end timestamp format, enhancing user experience.

--- delt/main.py
+++ delt/main.py
@@ -3,16 +3,19 @@ import re
 import arrow
 import typer
 from typing_extensions import Annotated
+from icecream import ic
+from typing import Optional
 
 app = typer.Typer()
 
 
-def format_duration(duration: int) -> str:
+def format_duration(duration: int, from_now: bool) -> str:
     """
     Given a time delta (in seconds), return a human-readable string.
     Consider if the seconds is a negative value, the time delta is in the future.
     format the time delta in a human-readable format using ago() or humanize() method.
     :param duration:    The time delta to format (in seconds).
+    :param from_now:    If True, return a relative time delta.
     :return: str        A human-readable string representing the time delta.
     """
     # Check if the duration is +- 5 seconds
@@ -23,12 +26,16 @@ def format_duration(duration: int) -> str:
     present = arrow.utcnow()
     delta = present.shift(seconds=duration)
 
+    if not from_now:
+        ic(from_now)
+        return f"{delta.humanize(only_distance=True)}."
+
     return (f"{'in ' if duration < 0 else ''}"
             f"{delta.humanize(only_distance=True)}"
             f"{' ago' if duration > 0 else ''}.")
 
 
-def calculate_delta_seconds(start: str, end: str) -> str:
+def calculate_delta_seconds(start: str, end: Optional[str] = None) -> str:
     """
     Calculate the elapsed time between two timestamps.
 
@@ -41,6 +48,11 @@ def calculate_delta_seconds(start: str, end: str) -> str:
     Returns:
         A string representing the elapsed time between the two timestamps.
     """
+    from_now = False
+    if end is None:
+        # If end is None, assume the end time is now
+        end = arrow.utcnow().format("YYYY-MM-DD HH:mm:ss")
+        from_now = True
 
     start_time = arrow.get(start)
     end_time = arrow.get(end)
@@ -49,8 +61,9 @@ def calculate_delta_seconds(start: str, end: str) -> str:
     duration = int(
         (end_time - start_time).total_seconds()
     )  # This will be positive if end_time is in the future. Negative if in the past.
+    # return the formatted duration, from_now should be True if end_time is not within 10 seconds of now.
 
-    return format_duration(duration)
+    return format_duration(duration, from_now)
 
 
 @app.command()
@@ -73,18 +86,14 @@ def main(
         ):
             # Assume the user forgot to quote the timestamps and concatenate them
             start, end = f"{start} {end}", None
-
     try:
-        # Parse the end timestamp or use the current time if not provided
-        end_time = arrow.now() if end is None else arrow.get(end)
 
         # Calculate the elapsed time
         elapsed_time = calculate_delta_seconds(
-            start, end_time.format("YYYY-MM-DD HH:mm:ss")
+            start, end
         )
 
-        end_time_str = end_time.format("YYYY-MM-DD HH:mm:ss")
-        typer.echo(f"Elapsed time from '{start}' to {end_time_str}:\n{elapsed_time}")
+        typer.echo(f"Elapsed time from '{start}' to {end if end is not None else arrow.utcnow().format('YYYY-MM-DD HH:mm:ss')}:\n{elapsed_time}")
 
     except arrow.parser.ParserError:
         # Handle parsing errors specifically related to arrow
